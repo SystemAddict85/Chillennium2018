08f664f746fffd642bce3893b228edfdf7dd9113
Brandon Anderson <brandanders85@tamu.edu>
2018-10-12 23:32:05 -0500

    simple idle and movement controller for two players

---
+++ Chillennium2018/Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerController : Controller
+{   
+    public ControllerType playerNumber = ControllerType.PLAYER_ONE;
+
+    protected override void Update()
+    {
+        string hor = "Horizontal1", vert = "Vertical1";
+        if (playerNumber == ControllerType.PLAYER_TWO)
+        {
+            hor = "Horizontal2";
+            vert = "Vertical2";
+        }
+        horizontal = Input.GetAxisRaw(hor);
+        vertical = Input.GetAxisRaw(vert);
+    }
+}

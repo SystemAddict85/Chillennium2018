7f8c04964a3fa4f056bd2abdfbb52256c88f17e4
BEN-DESKTOP\Ben <login1464@gmail.com>
2018-10-12 23:44:04 -0500

    Added scene transition

---
+++ Chillennium2018/Assets/Scripts/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+
+    public Button endButton;
+
+    void Start()
+    {
+        endButton = GetComponent<Button>();
+        endButton.onClick.AddListener(OnMouseClick);
+
+    }
+    public void OnMouseClick()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+
+}

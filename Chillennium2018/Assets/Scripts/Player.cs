788462525d3d8408cb3de3c5c1e1bff9c0aec6f9
Brandon Anderson <brandanders85@gmail.com>
2018-10-13 23:09:03 -0500

    animating room spawning

--- Chillennium2018/Assets/Scripts/Player.cs
+++ Chillennium2018/Assets/Scripts/Player.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class Player : Character {
 
+    public bool isPlayerDead = false;
+
     private void Awake()
     {
         maxHealth = 6f;
         currentHealth = maxHealth;
     }
+
+    public override void Die()
+    {
+        isPlayerDead = true;
+        if (!LevelManager.Instance.isAPlayerDead)
+        {
+            LevelManager.Instance.isAPlayerDead = true;
+        }
+        else
+        {
+            // TODO: GameOver
+        }
+
+    }
+
+    public void HidePlayer()
+    {
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
+    }
+
+    public void ShowPlayer()
+    {
+        GetComponentInChildren<SpriteRenderer>().enabled = true;
+    }
+
 
     public void PowerUp(Spell.SpellType spellType)
     {
